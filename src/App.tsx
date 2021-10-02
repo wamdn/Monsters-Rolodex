3e0a6d1163bc768506c818f53d0c5a5575406010
wail <wail.madrane@gmail.com>
2021-10-02 18:34:42 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Monster {
+  name: string;
+}
+
+interface AppState {
+  readonly monsters: Monster[];
+}
+
+class App extends Component<{}, AppState> {
+  state: AppState = {
+    monsters: [
+      { name: 'Frankenstein' },
+      { name: 'Frankenstein' },
+      { name: 'Frankenstein' }
+    ]
+  }
+  render() {
+    return (
+      <main className="App">
+
+      </main>
+    );
+  }
 }
 
 export default App;
